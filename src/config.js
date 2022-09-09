e9c4597cc8867e922b20592dfc006bf32a4af84f
Owen White <otwhite03@gmail.com>
2022-09-09 12:58:36 -0400

    Updated website contact

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     school_year: '21-22',
     membership_contact: "Kayli Bardy (bardy.2@osu.edu)",
     scholarship_contact: "Caroline Smith (smith.12364@osu.edu)",
-    website_contact: "Jeff Bonner (bonner.174@osu.edu)",
+    website_contact: "Owen White (white.3629@osu.edu)",
     sac_email: "osusacer@gmail.com",
     membership_enabled: false,
     scholarship_enabled: true,
