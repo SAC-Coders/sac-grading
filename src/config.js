ea7940448336b9a9ee6f1d7bb86e3e883c88d264
Jeff Bonner <jbonner50@gmail.com>
2022-10-07 14:27:01 -0400

    changes for membership 22-23

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 const config = {
-    school_year: '21-22',
+    school_year: '22-23',
     membership_contact: "Kayli Bardy (bardy.2@osu.edu)",
     scholarship_contact: "Caroline Smith (smith.12364@osu.edu)",
     website_contact: "Owen White (white.3629@osu.edu)",
     sac_email: "osusacer@gmail.com",
-    membership_enabled: false,
-    scholarship_enabled: true,
-    default_view: "Scholarship",
+    membership_enabled: true,
+    scholarship_enabled: false,
+    default_view: "Membership",
     question_word_limit: 350,
     membership_form: 'https://docs.google.com/forms/d/e/1FAIpQLSdtwVoIoOadLyTTKQzfp6saVUK4y3OFntRekAKFc5iURJlVWA/viewform',
-    scholarship_form: 'https://docs.google.com/forms/d/e/1FAIpQLSehp2Ovg0T9k6hfTpZF8MPgza2ygl2blZpCh8FqsU179Fzchw/viewform',
+    scholarship_form: '',
 }
 
 export default config
