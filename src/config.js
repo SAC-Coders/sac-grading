d55561932ff8cadecbf725ce4756df35bb438132
Owen White <otwhite03@gmail.com>
2023-02-14 19:07:05 -0500

    Scholarship updates 23

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const config = {
     default_view: "Scholarship",
     question_word_limit: 350,
     membership_form: 'https://docs.google.com/forms/d/e/1FAIpQLSe0RZ7couOPmduMzEkEoD2g8MoooSHvsw0YBX0Hb_D0Idwhrg/viewform?usp=sf_link',
-    scholarship_form: '',
+    scholarship_form: 'https://forms.gle/Fs4WQMSpB8xo4qBbA',
 }
 
 export default config
