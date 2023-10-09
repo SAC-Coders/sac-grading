ffac2cf265d8c6344034127bee5c8aa1e8fdbc6d
gabriel <petrigabo07@gmail.com>
2023-10-09 09:10:35 -0400

    Updated for 2024

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
-    school_year: '22-23',
-    membership_contact: "Sophia LaBianco (labianco.1@osu.edu)",
+    school_year: '23-24',
+    membership_contact: "Molly Spring (spring.131@osu.edu)",
     scholarship_contact: "Isaac Kantarovich (kantarovich.5@osu.edu)",
-    website_contact: "Owen White (white.3629@osu.edu)",
+    website_contact: "Gabe Petrarca (petrarca.26@osu.edu)",
     sac_email: "osusacer@gmail.com",
-    membership_enabled: false,
-    scholarship_enabled: true,
+    membership_enabled: true,
+    scholarship_enabled: false,
     default_view: "Scholarship",
     question_word_limit: 350,
-    membership_form: 'https://docs.google.com/forms/d/e/1FAIpQLSe0RZ7couOPmduMzEkEoD2g8MoooSHvsw0YBX0Hb_D0Idwhrg/viewform?usp=sf_link',
+    membership_form: 'https://forms.gle/FnVna5FtL2uYGTaM9',
     scholarship_form: 'https://forms.gle/Fs4WQMSpB8xo4qBbA',
 }
 
