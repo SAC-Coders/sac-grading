ae280bf8df8091ccbab47b5700d22cc28f5cca40
Gabriel <petrigabo07@gmail.com>
2024-02-05 13:38:02 -0500

    updated 2024

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 const config = {
     school_year: '23-24',
     membership_contact: "Molly Spring (spring.131@osu.edu)",
-    scholarship_contact: "Isaac Kantarovich (kantarovich.5@osu.edu)",
+    scholarship_contact: "Aaron	Gillespie (Gillespie.374@buckeyemail.osu.edu)",
     website_contact: "Gabe Petrarca (petrarca.26@osu.edu)",
     sac_email: "osusacer@gmail.com",
-    membership_enabled: true,
-    scholarship_enabled: false,
+    membership_enabled: false,
+    scholarship_enabled: true,
     default_view: "Scholarship",
     question_word_limit: 300,
     membership_form: 'https://forms.gle/FnVna5FtL2uYGTaM9',
-    scholarship_form: 'https://forms.gle/Fs4WQMSpB8xo4qBbA',
+    scholarship_form: 'https://docs.google.com/forms/d/1vuW9V8geyvsMf0y9RcyP_dMhDPTqkT01zjcJzi7wXvY/edit',
 }
 
 export default config
