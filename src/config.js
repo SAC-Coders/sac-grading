00f3520885a8e9f62f95d840c0c34cacede515f1
Gabriel-Petrarca <petrigabo07@gmail.com>
2024-10-07 10:14:16 -0400

    upadted 24-25 membership

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     school_year: '23-24',
-    membership_contact: "Molly Spring (spring.131@osu.edu)",
+    membership_contact: "Rachel Ostavitz (Ostavitz.2@osu.edu)",
     scholarship_contact: "Aaron	Gillespie (Gillespie.374@buckeyemail.osu.edu)",
     website_contact: "Gabe Petrarca (petrarca.26@osu.edu)",
     sac_email: "osusacer@gmail.com",
@@ -8,7 +8,7 @@ const config = {
     scholarship_enabled: true,
     default_view: "Scholarship",
     question_word_limit: 300,
-    membership_form: 'https://forms.gle/FnVna5FtL2uYGTaM9',
+    membership_form: 'https://docs.google.com/forms/d/1qnjPuDUx25mJwzF2zqlYN276o_yB_jvw3ZTpF8XZYrY/edit',
     scholarship_form: 'https://docs.google.com/forms/d/e/1FAIpQLSc9fu9QmlgmXmzXJIDA4qmeYHIsYuZXL9ck8pTgSrLYM4iyAw/viewform',
 }
 
