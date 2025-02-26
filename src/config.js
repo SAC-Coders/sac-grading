2a81b1020a3a87c3f5db1a2267406ca6bb425c1b
Gabriel-Petrarca <petrigabo07@gmail.com>
2025-02-26 08:21:07 -0500

    new form

--- src/config.js
+++ src/config.js
@@ -4,12 +4,12 @@ const config = {
     scholarship_contact: "Aaron	Gillespie (Gillespie.374@buckeyemail.osu.edu)",
     website_contact: "Gabe Petrarca (petrarca.26@osu.edu)",
     sac_email: "osusacer@gmail.com",
-    membership_enabled: true,
-    scholarship_enabled: false,
+    membership_enabled: false,
+    scholarship_enabled: true,
     default_view: "Scholarship",
     question_word_limit: 300,
     membership_form: 'https://docs.google.com/forms/d/1qnjPuDUx25mJwzF2zqlYN276o_yB_jvw3ZTpF8XZYrY/viewform?edit_requested=true',
-    scholarship_form: 'https://docs.google.com/forms/d/e/1FAIpQLSc9fu9QmlgmXmzXJIDA4qmeYHIsYuZXL9ck8pTgSrLYM4iyAw/viewform',
+    scholarship_form: 'https://docs.google.com/forms/d/e/1FAIpQLSfY2YMN18MVLclB3PDZpnZ9ufyCtxKVUj_xTHHhe-K-_mJu0w/viewform',
 }
 
 export default config
